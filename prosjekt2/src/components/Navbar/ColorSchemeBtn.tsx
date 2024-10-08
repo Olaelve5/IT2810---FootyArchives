16fdcde3772022b6f774196fbca4c4f52c3d5a57
Ola Johannes Elvedahl <olajel@stud.ntnu.no>
2024-10-08 12:00:39 +0200

    fix: small styling fixes to colorscheme button and navbar

--- prosjekt2/src/components/Navbar/ColorSchemeBtn.tsx
+++ prosjekt2/src/components/Navbar/ColorSchemeBtn.tsx
@@ -1,19 +1,20 @@
-
-import { useMantineColorScheme, Button } from '@mantine/core';
+import { useMantineColorScheme, Button, useMantineTheme } from '@mantine/core';
 import { IconSun, IconMoon } from '@tabler/icons-react';
 
 export default function ColorSchemeBtn() {
-    const { colorScheme, setColorScheme } = useMantineColorScheme({
-        keepTransitions: true,
-      });
-    
-    return (
-        <Button
-        onClick={() => setColorScheme(colorScheme === 'dark' ? 'light' : 'dark')}
-        color="primary"
-        style={{ height: '35px', width: '35px', padding: '0', border: 'none' }}
-        >
-        {colorScheme === 'dark' ? <IconSun size={20}/> : <IconMoon size={20}/>}
-        </Button>
-    );
+  const { colorScheme, setColorScheme } = useMantineColorScheme({
+    keepTransitions: true,
+  });
+  const isDark = colorScheme === 'dark';
+  const theme = useMantineTheme();
+
+  return (
+    <Button
+      color={isDark ? theme.colors.darkmode[2] : 'white'}
+      onClick={() => setColorScheme(isDark ? 'light' : 'dark')}
+      style={{ height: '35px', width: '35px', padding: '0', border: '1px solid', boxShadow: '0 0 10px -5px rgba(0, 0, 0, 0.5)' }}
+    >
+      {isDark ? <IconSun size={20} color='white'/> : <IconMoon size={20} color={theme.colors.dark[4]}/>}
+    </Button>
+  );
 }
