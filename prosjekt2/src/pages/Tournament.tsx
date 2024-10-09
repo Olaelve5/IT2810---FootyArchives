23b483ec6931ddb89acf85f7448044bb03a761a5
Ola Johannes Elvedahl <olajel@stud.ntnu.no>
2024-10-09 15:47:35 +0200

    fix: applied the layout to other pages
    #11

--- prosjekt2/src/pages/Tournament.tsx
+++ prosjekt2/src/pages/Tournament.tsx
@@ -1,21 +1,22 @@
-import { useParams } from "react-router-dom";
-import SideBar from "../components/SideBar/SideBar";
-import Navbar from "../components/Navbar/Navbar";
-import MatchcardCarousel from "../components/Carousel";
-
+import { useParams } from 'react-router-dom';
+import SideBar from '../components/SideBar/SideBar';
+import Navbar from '../components/Navbar/Navbar';
+import MatchcardCarousel from '../components/Carousel';
 
 export default function Tournament() {
-    const { tournamentName } = useParams<{ tournamentName: string }>();
+  const { tournamentName } = useParams<{ tournamentName: string }>();
 
-    return (
-        <div className='layoutContainer'>
-          <SideBar />
-          <div className='rightContainer'>
-            <Navbar />
-            <h1>{tournamentName}</h1>
-            <p>Info om {tournamentName}</p> 
-            <MatchcardCarousel />
-          </div>
+  return (
+    <div className="layoutContainer">
+      <SideBar />
+      <div className="rightContainer">
+        <div className="rightInnerContainer">
+          <Navbar />
+          <h1>{tournamentName}</h1>
+          <p>Info om {tournamentName}</p>
+          <MatchcardCarousel />
         </div>
-      );
+      </div>
+    </div>
+  );
 }
