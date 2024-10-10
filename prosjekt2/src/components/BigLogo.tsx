3e89f4fd5f8c915664ed1d9bf68d159af7eda387
Ola Johannes Elvedahl <olajel@stud.ntnu.no>
2024-10-10 15:01:46 +0200

    fix: fixed color of button on light mode
    #13

--- prosjekt2/src/components/BigLogo.tsx
+++ prosjekt2/src/components/BigLogo.tsx
@@ -1,10 +1,13 @@
 import classes from '../styles/Home/BigLogo.module.css';
+import { useMantineColorScheme } from '@mantine/core';
 
 export default function BigLogo() {
+  const { colorScheme } = useMantineColorScheme();
+  const isDark = colorScheme === 'dark';
   return (
     <div className={classes.container}>
       <div className={classes.logoContainer}>
-        <h1 className={classes.headerGreen}>Footy</h1>
+        <h1 className={classes.header} id={isDark ? classes.headerGreenDark : classes.headerGreenLight}>Footy</h1>
         <h1 className={classes.header}>Archives</h1>
       </div>
       <p className={classes.subHeader}>Discover and explore international matches from 1872 to 2024. </p>
