d2d284c1a711c406d97636795e84311b20993e4b
PeterEhrhardt <peter.ehrhardt0403@gmail.com>
2024-10-11 13:48:51 +0200

    fix: edited what carousel shows on tournament pages

--- prosjekt2/src/pages/Tournament.tsx
+++ prosjekt2/src/pages/Tournament.tsx
@@ -1,22 +1,36 @@
-import { useParams } from 'react-router-dom';
-import SideBar from '../components/SideBar/SideBar';
-import Navbar from '../components/Navbar/Navbar';
-import MatchcardCarousel from '../components/Carousel';
+import { useParams } from "react-router-dom";
+import SideBar from "../components/SideBar/SideBar";
+import Navbar from "../components/Navbar/Navbar";
+import MatchcardCarousel from "../components/Carousel";
+import classes from "../styles/Tournament/Tournament.module.css";
+
 
 export default function Tournament() {
-  const { tournamentName } = useParams<{ tournamentName: string }>();
+    const { tournamentName } = useParams<{ tournamentName: string }>();
 
-  return (
-    <div className="layoutContainer">
-      <SideBar />
-      <div className="rightContainer">
-        <div className="rightInnerContainer">
-          <Navbar />
-          <h1>{tournamentName}</h1>
-          <p>Info om {tournamentName}</p>
-          <MatchcardCarousel />
+    return (
+      <div className="layoutContainer">
+        <SideBar />
+        <div className="rightContainer">
+          <div className="rightInnerContainer">
+            <Navbar />
+            <h1>{tournamentName}</h1>
+            <p>Info om {tournamentName}</p>{' '}
+            <div className={classes.carouselSection}>
+              <h2>2022</h2>
+              <MatchcardCarousel />
+            </div>
+            <div className={classes.carouselSection}>
+              <h2>2018</h2>
+              <MatchcardCarousel />
+            </div>
+            <div className={classes.carouselSection}>
+              <h2>2014</h2>
+              <MatchcardCarousel />
+            </div>
+            {/* <DiscoverButton /> */}
+          </div>
         </div>
       </div>
-    </div>
-  );
+    );
 }
