9f2c0f65f7297ed7e1ecf90812e27c52168b5942
Ola Johannes Elvedahl <olajel@stud.ntnu.no>
2024-10-11 11:36:24 +0200

    fix: updated not found page to include language switch
    #15

--- prosjekt2/src/pages/NotFound.tsx
+++ prosjekt2/src/pages/NotFound.tsx
@@ -1,15 +1,21 @@
 import { Button } from '@mantine/core';
 import { Link } from 'react-router-dom';
+import { useLanguageStore } from '../stores/language-store';
 
 export default function NotFound() {
-    return (
-      <div>
-        <h1>Red Card!</h1>
-        <p>This page has been sent off—let's get you back on track.</p>
-        <Link to="/project2">
-          <Button color="primary">Go to the home page</Button>
-        </Link>
-      </div>
-    );
-  }
-  
+  const language = useLanguageStore((state) => state.language);
+  return (
+    <div>
+      <h1>{language === 'en' ? 'Red card!' : 'Rødt kort!'}</h1>
+      <p>
+        {language === 'en'
+          ? "This page has been sent off—let's get you back on track."
+          : 'Denne siden har blitt sendt av banen—la oss få deg tilbake på sporet.'}
+      </p>
+      <Link to="/project2">
+        {/* <Button color="primary">Go to the home page</Button> */}
+        <Button radius={100} color="primary">{language === 'en' ? 'Go back to the home page' : 'Gå tilbake til startsiden'}</Button>
+      </Link>
+    </div>
+  );
+}
