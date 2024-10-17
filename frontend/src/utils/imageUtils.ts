a1d8f7faaba4e19f5dec7626521cb324bd05296c
Ola Johannes Elvedahl <olajel@stud.ntnu.no>
2024-10-18 00:09:18 +0200

    fix: some country codes + default image
    #26

--- frontend/src/utils/imageUtils.ts
+++ frontend/src/utils/imageUtils.ts
@@ -3,7 +3,9 @@ import country_codes from '../assets/country_codes.json';
 export const getCountryCode = (countries: string[]) => {
   const countryCodes = countries.map((country) => {
     const countryObj = country_codes.find((obj) => obj.Name === country);
-    return countryObj?.Code.toLowerCase();
+    return (
+      countryObj ? countryObj?.Code.toLowerCase() : 'xx'
+    );
   });
 
   return countryCodes;
