71d973b67952b89a0ada501547c9e7bf3ce7ae1c
Ola Johannes Elvedahl <olajel@stud.ntnu.no>
2024-10-22 19:56:30 +0200

    fix: fixed animation for logo on sidebar collapse
    #33

--- frontend/src/components/SideBar/Logo.tsx
+++ frontend/src/components/SideBar/Logo.tsx
@@ -1,30 +1,37 @@
 import { Text } from '@mantine/core';
 import { useSidebarCollapseStore } from '../../stores/sidebar-collapse-store';
+import classes from '../../styles/SideBar/Sidebar.module.css';
 
 function Logo() {
   const { isCollapsed } = useSidebarCollapseStore();
 
   return (
-    <div style={{ display: 'flex', alignItems: 'center', padding: '0 2rem', marginBottom: '1rem', justifyContent: isCollapsed ? 'center' : '' }}>
-      {isCollapsed ? (
-        <Text size="xl" fw={700} c="primary">
-          F
-        </Text>
-      ) : (
-        <Text size="xl" fw={700} c="primary">
-          Footy
-        </Text>
-      )}
+    <div
+      style={{
+        display: 'flex',
+        alignItems: 'center',
+        padding: '0 2rem',
+        marginBottom: '1rem',
+      }}
+    >
+      <Text
+        size="xl"
+        fw={700}
+        c="primary"
+        id={classes.logoText}
+        className={!isCollapsed ? classes.visibleLogoTextF : classes.hiddenLogoTextF}
+      >
+        Footy
+      </Text>
 
-      {isCollapsed ? (
-        <Text size="xl" fw={700}>
-          A
-        </Text>
-      ) : (
-        <Text size="xl" fw={700}>
-          Archives
-        </Text>
-      )}
+      <Text
+        size="xl"
+        fw={700}
+        id={classes.logoText}
+        className={!isCollapsed ? classes.visibleLogoTextA : classes.hiddenLogoTextA}
+      >
+        Archives
+      </Text>
     </div>
   );
 }
