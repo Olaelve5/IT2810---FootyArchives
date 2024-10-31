7065ff2ffc2b09ce16645b9ee24b1392255958c7
Ola Johannes Elvedahl <olajel@stud.ntnu.no>
2024-10-31 11:10:57 +0100

    fix: laoder icon in the correct place on result page + country code fix

--- frontend/src/pages/Result.tsx
+++ frontend/src/pages/Result.tsx
@@ -5,7 +5,7 @@ import SideBar from '../components/SideBar/SideBar';
 import MatchScorers from '../components/Matchup/MatchScorers';
 import MatchComments from '../components/Matchup/MatchComments';
 import { useNavigate } from 'react-router-dom';
-import { Group } from '@mantine/core';
+import { Group, Loader, useMantineTheme } from '@mantine/core';
 import { useParams } from 'react-router-dom';
 import { GET_RESULT } from '../graphql/queries';
 import { useQuery } from '@apollo/client';
@@ -16,6 +16,7 @@ export default function Result() {
   const navigate = useNavigate();
   const { resultId } = useParams<{ resultId: string }>();
   const { isCollapsed } = useSidebarCollapseStore();
+  const theme = useMantineTheme();
   const { loading, error, data } = useQuery(GET_RESULT, {
     variables: { id: resultId },
   });
@@ -27,7 +28,6 @@ export default function Result() {
     window.scrollTo(0, 0);
   }, []);
 
-  if (loading) return <p>Loading...</p>;
   if (error) navigate('/project2/not-found');
 
   return (
@@ -36,12 +36,16 @@ export default function Result() {
       <div id="rightContainer" className={isCollapsed ? 'rightContainerCollapsed' : 'rightContainerExpanded'}>
         <div className="rightInnerContainer">
           <Navbar />
-          <Group gap={10}>
-            <MatchDetails {...result} />
-            <MatchScore {...result} />
-            <MatchScorers result={result} />
-            <MatchComments result={result} />
-          </Group>
+          {loading ? (
+            <Loader size={30} color={theme.colors.primary[9]}/>
+          ) : (
+            <Group gap={10}>
+              <MatchDetails {...result} />
+              <MatchScore {...result} />
+              <MatchScorers result={result} />
+              <MatchComments result={result} />
+            </Group>
+          )}
         </div>
       </div>
     </div>
