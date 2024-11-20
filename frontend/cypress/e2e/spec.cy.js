0329da27a59f121c595f6df1044b4065d14d3611
Ola Johannes Elvedahl <olajel@stud.ntnu.no>
2024-11-20 09:50:19 +0100

    feat: navigation e2e tests finished
    #73

--- frontend/cypress/e2e/spec.cy.js
+++
@@ -1,9 +0,0 @@
-describe('My First Test', () => {
-  it('Visits Brasil page', () => {
-    cy.visit('http://localhost:5173/project2')
-
-    cy.contains('Brazil').click()
-
-    cy.url().should('include', '/project2/nation/Brazil')
-  })
-})
