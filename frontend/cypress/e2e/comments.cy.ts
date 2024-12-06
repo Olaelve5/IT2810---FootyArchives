65948cab4e23d72c1d5d63797b2bf58d0bece539
Ola Johannes Elvedahl <olajel@stud.ntnu.no>
2024-12-06 16:05:14 +0100

    fix: updated comment e2e tests to function with new format in db
    #106

--- frontend/cypress/e2e/comments.cy.ts
+++ frontend/cypress/e2e/comments.cy.ts
@@ -2,75 +2,79 @@
 
 // This file tests the comments on matchups sites with mocked data
 describe('Test of comments on the matchup page', () => {
-
-    beforeEach(() => {
-      // Intercept the getComments GraphQL query
-      cy.intercept('POST', '/graphql', (req) => {
-        if (req.body.operationName === 'getComments') {
-          req.reply({
-            fixture: 'getComments.json' // Respond with the mocked data
-          });
-        }
-      }).as('getComments');
-    });
-  
-    it('should display comments', () => {
-      // Visit the page where comments are displayed
-      cy.visit('/matchup/670d142ccc20a61eeae743ea');
-      
-      // Assert that the mocked comments are displayed
-      cy.contains('John Doe');
-      cy.contains('This is a test comment.');
-      cy.contains('Jane Smith');
-      cy.contains('Another test comment.');
-    });
-  
-    it('should allow posting a new comment and display it', () => {
-        const resultID = '670d142ccc20a61eeae743ea';
-      // Intercept the AddComment mutation
-      cy.intercept('POST', '/graphql', (req) => {
-        if (req.body.operationName === 'AddComment') {
-          // Verify the request payload matches expectations
-          expect(req.body.variables).to.deep.equal({
-            resultId: resultID,
-            comment: 'Canada good',
-            userName: 'canadian-sausage',
-          });
-  
-          // Mock the server's response
-          req.reply({
-            data: {
-              addComment: {
-                user_name: 'canadian-sausage',
-                date: new Date().toISOString(),
-                comment: 'Canada good',
-                result_id: resultID,
-                __typename: 'Comment',
-              },
-            },
-          });
-        }
-      }).as('AddComment');
-  
-      // Visit the page
-      cy.visit(`/matchup/${resultID}`);
-  
-      // Open the comment modal
-      cy.contains('Add comment').click();
-  
-      // Fill out the comment form
-      cy.get('input[aria-label="Enter your username"]').type('canadian-sausage');
-      cy.get('textarea[aria-label="Write a comment"]').type('Canada good');
-  
-      // Submit the comment
-      cy.contains('button', 'Post comment').click();
-  
-      // Wait for the AddComment mutation
-      cy.wait('@AddComment');
-  
-      // Assert the new comment appears on the page
-      cy.contains('canadian-sausage');
-      cy.contains('Canada good');
-    });
+  beforeEach(() => {
+    // Intercept the getComments GraphQL query
+    cy.intercept('POST', '/graphql', (req) => {
+      if (req.body.operationName === 'getComments') {
+        req.reply({
+          fixture: 'getComments.json', // Respond with the mocked data
+        });
+      }
+    }).as('getComments');
   });
-  
+
+  it('should display comments', () => {
+    // Visit the page where comments are displayed
+    cy.visit('/matchup/674c853c1dde3e61ff21077d');
+
+    // Assert that the mocked comments are displayed
+    cy.contains('John Doe');
+    cy.contains('This is a test');
+    cy.contains('Jane Smith');
+    cy.contains('This is another test');
+  });
+
+  it('should allow posting a new comment and display it', () => {
+    const resultID = '674c853c1dde3e61ff21077d';
+    const userID = '6750569e4dd64d6a93001c99';
+
+    // Intercept the AddComment mutation
+    cy.intercept('POST', '/graphql', (req) => {
+      if (req.body.operationName === 'AddComment') {
+        // Verify the request payload matches expectations
+        expect(req.body.variables).to.deep.equal({
+          resultId: resultID,
+          comment: 'Canada good',
+          username: 'canadaman',
+        });
+
+        // Mock the server's response
+        req.reply({
+          data: {
+            addComment: {
+              id: '6750ba58460a90bf99251afb',
+              user: {
+                id: userID,
+                username: 'canadaman',
+              },
+              date: new Date().toISOString(),
+              comment: 'Canada good',
+              result_id: resultID,
+              __typename: 'Comment',
+            },
+          },
+        });
+      }
+    }).as('AddComment');
+
+    // Visit the page
+    cy.visit(`/matchup/${resultID}`);
+
+    // Open the comment modal
+    cy.contains('Add comment').click();
+
+    // Fill out the comment form
+    cy.get('input[aria-label="Enter your username"]').type('canadaman');
+    cy.get('textarea[aria-label="Write a comment"]').type('Canada good');
+
+    // Submit the comment
+    cy.contains('button', 'Post comment').click();
+
+    // Wait for the AddComment mutation
+    cy.wait('@AddComment');
+
+    // Assert the new comment appears on the page
+    cy.contains('canadaman');
+    cy.contains('Canada good');
+  });
+});
